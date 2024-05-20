0e3dc231105983ad7c0da77d357271ff958e3d42
Felipe DR <felipe.corp7@gmail.com>
2024-05-20 14:41:19 -0300

    feat: class full

---
+++ src/theme/theme-provider.tsx
@@ -0,0 +1,7 @@
+import { ThemeProvider as StyledThemeProvider } from "styled-components";
+
+import theme from "./theme";
+
+export default function ThemeProvider({ children }) {
+  return <StyledThemeProvider theme={theme}>{children}</StyledThemeProvider>;
+}
